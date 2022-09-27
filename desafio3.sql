9901c5ef8cbd1569ecea4a956ee44f27c768e5ad
thais viana <thaisviana081@outlook.com>
2022-09-27 08:42:39 -0300

    03 - Exibe o histórico de reprodução para cada pessoa usuária 😅 ✔

---
+++ desafio3.sql
@@ -0,0 +1,10 @@
+SELECT SpotifyClone.usuarios.usuario AS usuario, 
+COUNT(SpotifyClone.historico_de_reproducoes.usuario_id) AS qt_de_musicas_ouvidas,
+ROUND(SUM(SpotifyClone.cancoes.duracao) / 60, 2) AS total_minutos 
+FROM SpotifyClone.usuarios 
+INNER JOIN SpotifyClone.historico_de_reproducoes 
+ON SpotifyClone.usuarios.usuario_id = SpotifyClone.historico_de_reproducoes.usuario_id
+INNER JOIN SpotifyClone.cancoes 
+ON SpotifyClone.cancoes.cancao_id = SpotifyClone.historico_de_reproducoes.cancao_id 
+GROUP BY usuario 
+ORDER BY usuario;
